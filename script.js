3ca9648498f1a91b2952d8987a8e66b809d09b5e
Nupur Sikka <67430495+Nupur2001@users.noreply.github.com>
2024-07-16 20:41:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -52,3 +52,28 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+// Product Images Switch
+
+const allHoverImages=document.querySelectorAll('.hover-container div img')
+const imgContainer=document.querySelector('.img-container')
+
+window.addEventListener('DOMContentLoaded',()=>{
+    allHoverImages[0].parentElement.classList.add('.active')
+});
+
+allHoverImages.forEach((Image)=>{
+    Image.addEventListener('mouseover',()=>{
+        // console.log(image.src)
+        imgContainer.querySelector('img').src=Image.src;
+        resetActiveImg();
+        Image.parentElement.classList.add('active');
+    });
+});
+
+function resetActiveImg(){
+    allHoverImages.forEach((img)=>{
+        img.parentElement.classList.remove('active')
+    })
+}
+
